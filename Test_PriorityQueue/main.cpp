7b98ba3ce910ffe23ebf5a9b42cf811fcd47494e
Linh Hoang-Tuan <linhhoangtuan95@gmail.com>
2020-05-05 17:51:25 +0900

    update Test_PriorityQueue project

--- Test_PriorityQueue/main.cpp
+++ Test_PriorityQueue/main.cpp
@@ -1,10 +1,14 @@
+/* Using priority queue in Cpp
+Note: priority_queue in Cpp adapts the vector container to maintain items in an sorted order.
+*/
 #include <iostream>
 #include <queue>
+#include <bits/stdc++.h>
+#include <vector>
 
 using namespace std;
 
-class edge{
-public:
+struct edge{
     int src;
     int des;
     int wei;
@@ -16,10 +20,27 @@ public:
 
 int main()
 {
-    priority_queue<edge> ique;
+    /*using priority_queue*/
     edge e1(0, 1, 1), e2(0, 2, 2), e3(1, 2, 3);
+    priority_queue<edge> ique;
     ique.push(e1); ique.push(e2); ique.push(e3);
     edge e = ique.top();
     cout << e.src << " " << e.des << " " << e.wei << "\n";
+
+//    /* Second approach: using vector, to extract the minimum element -> use *min_element function */
+//    edge e1(0, 1, 1), e2(0, 2, 2), e3(1, 2, 3);
+//    vector<edge> myvec;
+//    myvec.push_back(e1);
+//    myvec.push_back(e2);
+//    myvec.push_back(e3);
+//    cout << "min edge: \n";
+//    edge me = *min_element(myvec.begin(), myvec.end());
+//    cout << me.src << " " << me.des << " " << me.wei << "\n";
+//    myvec.erase(myvec.begin());
+//    cout << "after erase:\n";
+//    for (int i = 0; i < myvec.size(); i++) {
+//        cout << myvec[i].src << " " << myvec[i].des << " " << myvec[i].wei << endl;
+//    }
+
     return 0;
 }
