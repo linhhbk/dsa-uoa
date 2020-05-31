6ad8b3ddd118c113ffe2ecac49ab271bd0e5c973
Linh Hoang-Tuan <linhhoangtuan95@gmail.com>
2020-05-31 09:41:20 +0900

    minor updates

--- TestProject/main.cpp
+++ TestProject/main.cpp
@@ -1,9 +1,39 @@
 #include <iostream>
+#include<vector>
+#include<bits/stdc++.h>
 
 using namespace std;
 
+int mynumber = 0;
+int add(int x) {
+    if (x == 10) {
+        return mynumber;
+    }
+    else {
+        mynumber += add(x);
+    }
+}
+
+void test(int x) {
+    if (x==0) {
+        cout << x << "\n";
+        return;
+    }
+    cout << x+1 << "\n";
+}
+
+set<int> mylist;
+
 int main()
 {
-    cout << "Hello world!" << endl;
+    //cout << add(20);
+    // test(1);
+    mylist.insert(1);
+    if (mylist.find(1)== mylist.end()) {
+        cout << "error!" << endl;
+    }
+    else {
+        cout << "no element!\n";
+    }
     return 0;
 }
